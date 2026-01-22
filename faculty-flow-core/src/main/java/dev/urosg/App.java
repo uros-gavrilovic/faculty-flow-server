f5c22692c58e1a828206bb08780d2b002bfed492
Uros Gavrilovic <uros.gavrilovic@plusplusnt.rs>
2026-01-22 14:52:22 +0100

    [HEAD] (P) - Project initialization

---
+++ faculty-flow-core/src/main/java/dev/urosg/App.java
@@ -0,0 +1,10 @@
+package dev.urosg;
+
+/**
+ * Hello world!
+ */
+public class App {
+    static void main() {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/dev/urosg/Main.java
@@ -0,0 +1,7 @@
+package dev.urosg;
+
+public class Main {
+	static void main() {
+		System.out.println("Hello World!");
+	}
+}
