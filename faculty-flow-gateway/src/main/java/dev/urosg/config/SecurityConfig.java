bc1c56ecd7a5fb6798fb7271180768df4f02b5ad
uros-gavrilovic <uros.gavrilovic@plusplusnt.rs>
2026-03-25 23:48:26 +0100

    [develop] (P) - Gateway skeleton implementation

--- faculty-flow-gateway/src/main/java/dev/urosg/config/SecurityConfig.java
+++ faculty-flow-gateway/src/main/java/dev/urosg/config/SecurityConfig.java
@@ -1,22 +1,22 @@
-package dev.urosg.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@Configuration
-public class SecurityConfig {
-
-	@Bean
-	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-		http
-			.csrf(ServerHttpSecurity.CsrfSpec::disable)
-			.authorizeExchange(auth -> auth
-				.pathMatchers("/public/**").permitAll()
-				.anyExchange().authenticated()
-			)
-			.httpBasic(basic -> {});
-		return http.build();
-	}
-}
+//package dev.urosg.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//	@Bean
+//	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+//		http
+//			.csrf(ServerHttpSecurity.CsrfSpec::disable)
+//			.authorizeExchange(auth -> auth
+//				.pathMatchers("/public/**").permitAll()
+//				.anyExchange().authenticated()
+//			)
+//			.httpBasic(basic -> {});
+//		return http.build();
+//	}
+//}
