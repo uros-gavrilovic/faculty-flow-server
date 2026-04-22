a09481acc3febf047df0f9d77c40a21f68d36ca3
uros-gavrilovic <uros.gavrilovic@plusplusnt.rs>
2026-04-22 23:13:39 +0200

    [develop] (P) - New 'faculty-flow-reservation' module

---
+++ faculty-flow-reservation/src/main/java/dev/urosg/FacultyFlowReservation.java
@@ -0,0 +1,11 @@
+package dev.urosg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyFlowReservation {
+	static void main() {
+		SpringApplication.run(FacultyFlowReservation.class);
+	}
+}
---
+++ faculty-flow-reservation/src/main/java/dev/urosg/kafka/KafkaTopics.java
@@ -0,0 +1,4 @@
+package dev.urosg.kafka;
+
+public class KafkaTopics {
+}
